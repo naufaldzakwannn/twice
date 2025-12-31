353ccf226f8746789f86f9d20db7c80ab1224ed6
naufaldzakwannn <naufaldzakwan189@gmail.com>
2026-01-01 02:00:32 +0700

    Adding Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "TWICE Fan Web",
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="bg-pink-50">
+        <Analytics />
         <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />
